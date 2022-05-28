c97d141d776094667a419432d32ccfc2bbde91d9
badjateamisha <amishabadjate175@gmail.com>
2022-05-28 20:00:19 +0530

    Removing person details

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -9,7 +9,7 @@ CreatingContacts contact = new CreatingContacts();
 
 while (true)
 {
-    Console.WriteLine("Enter the number :\n1)Creating Contacts\n2)Edit Details\n3)Output Details");
+    Console.WriteLine("Enter the number :\n1)Creating contacts\n2)Edit Detaild\n3)Removing Details\n4)Output Details");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
@@ -17,9 +17,12 @@ while (true)
             contact.Contacts();
             break;
         case 2:
-            contact.edit();
+            contact.EditDetails();
             break;
         case 3:
+            contact.RemoveContact();
+            break;
+        case 4:
             contact.output();
             break;
     }
